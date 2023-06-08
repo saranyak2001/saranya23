6db5dcb5eb0e339950e0e3b7d65278972fe1dce5
saranya <saranyak1007@gmail.com>
2023-06-08 06:58:26 -0700

    saranya code

---
+++ src/test/java/projectgithub/prj1.java
@@ -0,0 +1,11 @@
+package projectgithub;
+
+public class prj1 {
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("saranyak");
+		System.out.println("good eveing");
+	}
+
+}
